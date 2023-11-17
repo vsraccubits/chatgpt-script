0287b93cd21fb6b949c9d098b038780049e9fb32
Accubits <accubits@Accubitss-MacBook-Pro-2.local>
2023-11-17 11:10:46 +0530

    fix: save to json file

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from chatgpt import chat_gpt
 from config.logger import logger
+import json
 
 prompt = """"
 Generate an example JSON.
@@ -12,3 +13,7 @@ response = chat_gpt.generate_json_response(prompt)
 logger.info("Response----------------------------------")
 logger.info(response)
 logger.info("------------------------------------------")
+
+# save response to file
+with open('response.json', 'w') as outfile:
+    json.dump(response, outfile, indent=4)
